efbb11b0d3a7bf43587fbaaa4dfb294ab6df72ea
Alasdair North <alnorth@gmail.com>
2011-05-01 16:29:10 +0100

    Very basic website with analytics.

---
+++ about.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>When is Dawn?</title>
+		<link rel="stylesheet" href="style.css" type="text/css" />
+		<?php include('hidden/ga.php'); ?>
+	</head>
+	<body>
+		<div class="wrapper">
+			<div id="about">
+				<p>This website doesn't work yet, but when it does it will tell you when the 'crack of dawn' is wherever you are.</p>
+			</div>
+			<div class="push"></div>
+		</div>
+		<div id="footer">
+			by <a href="http://www.twitter.com/alasdairnorth">@alasdairnorth</a> | <a href="about.php">about</a>
+		</div>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>When is Dawn?</title>
+		<link rel="stylesheet" href="style.css" type="text/css" />
+		<?php include('hidden/ga.php'); ?>
+	</head>
+	<body>
+		<div class="wrapper">
+			<div id="dawn">
+				<span class="time">9:41</span>
+				<span class="ampm">am</span>
+				<span class="timezone">BST</span>
+			</div>
+			<div class="push"></div>
+		</div>
+		<div id="footer">
+			by <a href="http://www.twitter.com/alasdairnorth">@alasdairnorth</a> | <a href="about.php">about</a>
+		</div>
+	</body>
+</html>
